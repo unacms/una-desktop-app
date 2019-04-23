16902b025ed98afdc1ade8dacd7adafcf895958d
AlexT <at@una.io>
2019-04-23 10:49:03 +0300

    Changes for Win OS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { app, session, shell, nativeImage, ipcMain, BrowserWindow } from 'electron';
+import { app, session, shell, nativeImage, ipcMain, BrowserWindow, Tray } from 'electron';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -7,6 +7,9 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 
 const BASE_URL = 'https://una.io/'; // 'http://hihi.una.io/'; 
 const TEMPLATE = 'protean';
+
+let tray;
+let win;
 
 function createWindow () {  
 
@@ -17,12 +20,12 @@ function createWindow () {
     });
     
     // create window
-    let win = new BrowserWindow({ 
+    win = new BrowserWindow({ 
         width: 800, 
         minWidth: 320, 
         height: 600, 
         minHeight: 480,
-        icon: __dirname + '/res/AppIcon.png', // for win and linux only
+        icon: __dirname + '/../res/AppIcon.png', // for win and linux only
     });
 
     // make external links to open in default browser
@@ -32,14 +35,33 @@ function createWindow () {
     });
     
     win.loadURL(`${BASE_URL}?skin=${TEMPLATE}`);
-    //win.webContents.openDevTools();
+    // win.webContents.openDevTools();
+
+	tray = new Tray(__dirname + '/../res/tray.png');
+	tray.setTitle('UNA.IO Messenger');
+	tray.setToolTip('UNA.IO Messenger (double-click - hide, click - focus)');
+	tray.on('double-click', () => {
+		win.isVisible() ? win.hide() : win.show()
+	});
+	tray.on('click', () => {
+		if (win.isVisible()) {
+			win.focus();
+		} 
+		else {
+			win.show();
+			win.focus();
+		}
+	});
 }
 
 ipcMain.on('message', (event, msg) => {    
-    if (msg.bubbles && msg.bubbles['notifications-messenger']) {
-        app.setBadgeCount(parseInt(msg.bubbles['notifications-messenger']));
+    if (msg.bubbles && msg.bubbles['notifications-messenger'] && parseInt(msg.bubbles['notifications-messenger']) > 0) {
+		let n = parseInt(msg.bubbles['notifications-messenger']);
+        app.setBadgeCount(n);
+		tray.setImage(__dirname + (n > 0 && n <= 3 ? '/../res/tray-' + n + '.png' : '/../res/tray-new.png'));
     } else {
         app.setBadgeCount(0);
+		tray.setImage(__dirname + '/../res/tray.png');
     }
 });
 
